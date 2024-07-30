232afd559eff39e28022db84c401da7ba71f8af6
Viresh Panchal <panchalviresh757@gmail.com>
2024-07-30 19:45:43 +0530

    upload

---
+++ portfolio/base/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from base.views import home
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", home, name='home'),
+] + static(settings.STATIC_URL)
