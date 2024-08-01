b316f8fb2ceeae9560a8603cb540dec481f405e2
Viresh Panchal <panchalviresh757@gmail.com>
2024-08-01 16:42:53 +0530

    update

--- portfolio/base/urls.py
+++ portfolio/base/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from base.views import home
+from base.views import home, demo
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name='home'),
+    path("demo/", demo, name='demo'),
 ] + static(settings.STATIC_URL)
--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {})
+
+
+def demo(request):
+    return render(request, "demo.html", {})
